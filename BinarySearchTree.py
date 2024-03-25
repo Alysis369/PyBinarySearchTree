27d4c8e32d76af221f9220ac5832885ac6e3d50f
alysis <aldosiswanto369@gmail.com>
2024-03-24 19:52:55 -0700

    Completed BST and BSTTest, with large n testing. Created readme with delete pseudocode.

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,93 +1,149 @@
-from typing import Any
+import warnings
 
 
 class Node:
-    def __init__(self, val: int = None, left: 'Node' = None, right: 'Node' = None):
+    def __init__(self, val=None, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
 
 class BST:
-    def __init__(self, root_value: int):
-        self.root = Node(val=root_value)
+    def __init__(self, root_val: int):
+        self.root = Node(val=root_val)
 
-    def insert(self, val: Any):
-        curr = self.root
-
-        while True:
-            if val < curr.val:
-                if curr.left:
-                    # move to left node
-                    curr = curr.left
-                else:
-                    # place new node here
-                    curr.left = Node(val=val)
-                    break
-
-            else:
-                if curr.right:
-                    # move to the right node
-                    curr = curr.right
-                else:
-                    # place new node here
-                    curr.right = Node(val=val)
-                    break
-
-    def delete(self, val: int):
-        # find the node
-        target = self.find(val)
-
-        # TODO: EC for right does not exist
-        curr, prev = target.right, None
-
-        # find smallest node
-        while curr.left:
-            curr, prev = curr.left, curr
-
-        # change the target node value
-        target.val = curr.val
-        prev.left = curr.right
-
-    def find(self, val: int) -> Node:
+    def insert(self, val: int):
         curr = self.root
 
         while curr:
             if val < curr.val:
-                curr = curr.left  # go left
+                # turn left
+                if curr.left:
+                    curr = curr.left
+                else:
+                    curr.left = Node(val=val)
+                    break
             elif val > curr.val:
-                curr = curr.right  # go right
+                # turn right
+                if curr.right:
+                    curr = curr.right
+                else:
+                    curr.right = Node(val=val)
+                    break
             else:
-                # found the node
-                return curr
+                # found an identical value
+                warnings.warn(f'Node with value {val} already exists!')
+                break
 
-        raise KeyError('Value not found in BST.')
+    def read(self) -> list:
+        read_list = []
+        if self.root:
+            self.read_recursive(self.root, read_list)
 
-    def bfs_traverse(self):
-        # use queue method
-        q = [self.root]
-        traverse_list = []
-        while q:
-            curr = q.pop(0)
+        return read_list
 
-            if curr:  # ignore empty ends
-                traverse_list.append(curr.val)
+    def read_recursive(self, root: Node, read_list: list):
+        if root.left:
+            self.read_recursive(root.left, read_list)
 
-                # put children to q
-                q.append(curr.left)
-                q.append(curr.right)
+        read_list.append(root.val)
 
-        return traverse_list
+        if root.right:
+            self.read_recursive(root.right, read_list)
 
-    def dfs_traverse(self):
-        traverse_list = []
+    def delete(self, val: int):
+        curr, prev = self.root, None
+        target = None
 
-        self.dfs_recursive(self.root, traverse_list)
+        # find the target node
+        while curr:
+            if val < curr.val:
+                # go left
+                curr, prev = curr.left, curr
+            elif val > curr.val:
+                # go right
+                curr, prev = curr.right, curr
+            else:
+                # found the node!
+                target = curr
+                break
 
-        return traverse_list
+        # if target is root node
+        if target == self.root:
+            if target.right and target.left:
+                curr_min = target.right
 
-    def dfs_recursive(self, root: Node, traverse_list: list):
-        if root:
-            self.dfs_recursive(root.left, traverse_list)
-            traverse_list.append(root.val)
-            self.dfs_recursive(root.right, traverse_list)
+                while curr_min.left:
+                    curr_min = curr_min.left
+
+                self.delete(curr_min.val)
+
+                # set the target's val to curr_min value
+                target.val = curr_min.val
+                return
+            elif target.left:
+                self.root = target.left
+                return
+            elif target.right:
+                self.root = target.right
+                return
+            else:
+                # empty tree
+                self.root = None
+                return
+
+        # if target is not found
+        if not curr:
+            raise Exception(f'Node with value {val} cannot be find in the tree!')
+
+        # leaf node case
+        if not target.left and not target.right:
+            if prev.left == target:
+                prev.left = None
+                return
+            else:
+                prev.right = None
+                return
+
+        # One child to the left
+        if not target.right:
+            if prev.left == target:
+                prev.left = target.left
+                return
+            else:
+                prev.right = target.left
+                return
+
+        # One child to the right
+        if not target.left:
+            if prev.left == target:
+                prev.left = target.right
+                return
+            else:
+                prev.right = target.right
+                return
+
+        # Target has both children
+        # find the smallest node on the right side of the target node
+        curr_min = target.right
+
+        while curr_min.left:
+            curr_min = curr_min.left
+
+        # re-delete with the new target being the that smallest curr_min val
+        # in case that curr_min val has children
+        self.delete(curr_min.val)
+
+        # set the target's val to curr_min value
+        target.val = curr_min.val
+
+
+if __name__ == '__main__':
+    bst = BST(10)
+    bst.insert(5)
+    bst.insert(7)
+    bst.insert(6)
+    bst.insert(15)
+    bst.insert(12)
+
+    bst.delete(6)
--- BinarySearchTreeTest.py
+++ BinarySearchTreeTest.py
@@ -1,52 +1,55 @@
 import unittest
 from BinarySearchTree import BST
+from random import randint, shuffle
 
 
 class BSTTestCase(unittest.TestCase):
-    def testInsert(self):
-        bst = BST(10)
-        bst.insert(2)
-        bst.insert(12)
-        bst.insert(4)
-        bst.insert(3)
-        bst.insert(5)
-
-        self.assertEqual(10, bst.root.val)
-        self.assertEqual(2, bst.root.left.val)
-        self.assertEqual(12, bst.root.right.val)
-        self.assertEqual(4, bst.root.left.right.val)
-        self.assertEqual(3, bst.root.left.right.left.val)
-
-    def testBfsTraverse(self):
-        bst = BST(10)
-        bst.insert(2)
-        bst.insert(12)
-        bst.insert(4)
-        bst.insert(3)
-        bst.insert(5)
-
-        self.assertEqual([10, 2, 12, 4, 3, 5], bst.bfs_traverse())
-
-    def testFind(self):
-        bst = BST(10)
-        bst.insert(2)
-        bst.insert(12)
-        bst.insert(4)
-        bst.insert(3)
-        bst.insert(5)
-
-        node = bst.find(4)
-        self.assertEqual(3, node.left.val)
-        self.assertEqual(5, node.right.val)
-
-    def testDelete(self):
+    def test_insert(self):
         bst = BST(10)
         bst.insert(5)
+        bst.insert(7)
+
+        self.assertEqual(5, bst.root.left.val)
+        self.assertEqual(7, bst.root.left.right.val)
+
+    def test_read(self):
+        bst = BST(10)
+        bst.insert(5)
+        bst.insert(7)
+        bst.insert(6)
         bst.insert(15)
         bst.insert(12)
-        bst.insert(13)
-        bst.delete(10)
 
+        self.assertEqual([5, 6, 7, 10, 12, 15], bst.read())
+
+    def test_delete(self):
+        bst = BST(10)
+        bst.insert(5)
+        bst.insert(7)
+        bst.insert(6)
+        bst.insert(15)
+        bst.insert(12)
+
+        bst.delete(6)
+        bst.delete(12)
+        self.assertEqual([5, 7, 10, 15], bst.read())
+
+    def test_massive_n(self):
+        bst = BST(50)
+        for _ in range(10):
+            bst.insert(randint(1, 101))
+
+        existing = bst.read()
+        shuffle(existing)
+        print(existing)
+
+        for _ in range(len(existing)):
+            print(existing)
+            element = existing.pop()
+
+            print(bst.root.val)
+            bst.delete(element)
+            self.assertEqual(sorted(existing), bst.read())
 
 
 if __name__ == '__main__':
